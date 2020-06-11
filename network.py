d8543db2e4a53f886f9c661015b12e5d03dec501
rad6 <heydarirad_hadi@yahoo.com>
2020-06-11 11:27:26 +0430

    results also separated by agent types

--- network.py
+++ network.py
@@ -30,34 +30,44 @@ def readAndParseAllData():
             for line in lines:
                 if "cwnd_" in line:
                     splt = line.split()
+                    splt.append(type_)
                     splt.append(i)
                     cwnd.append(splt)
                 elif "rtt_" in line:
                     splt = line.split()
+                    splt.append(type_)
                     splt.append(i)
                     rtt.append(splt)
                 elif line[0] == '+':
                     splt = line.split()
+                    splt.append(type_)
                     splt.append(i)
                     enqueued.append(splt)
                 elif line[0] == '-':
                     splt = line.split()
+                    splt.append(type_)
                     splt.append(i)
                     dequeued.append(splt)
                 elif line[0] == 'r':
                     splt = line.split()
+                    splt.append(type_)
                     splt.append(i)
                     received.append(splt)
                 elif line[0] == 'd':
                     splt = line.split()
+                    splt.append(type_)
                     splt.append(i)
                     dropped.append(splt)
                 
 
 def calculateAvgRtt():
     global rtt
-    flow1_result = []
-    flow2_result = []
+    newreno_f1 = []
+    newreno_f2 = []
+    tahoe_f1 = []
+    tahoe_f2 = []
+    vegas_f1 = []
+    vegas_f2 = []
     curr_vals = [0] * n_run
     rtt = sorted(rtt) # sort by time
     # for each in rtt:
@@ -67,34 +77,74 @@ def calculateAvgRtt():
         time_ = float(each[0])
         rtt_ = float(each[6])
         src = int(each[1])
+        type_ = each[7]
         run_id = int(each[len(each) - 1])
         curr_vals[run_id - 1] = rtt_
         avg = sum(curr_vals) / len(curr_vals)
-        flow1_result.append([time_, avg]) if (src == 0) else flow2_result.append([time_, avg])
-    return flow1_result, flow2_result
+        if type_ == "Newreno":
+            newreno_f1.append([time_, avg]) if (src == 0) else newreno_f2.append([time_, avg])
+        elif type_ == "Tahoe":
+            tahoe_f1.append([time_, avg]) if (src == 0) else tahoe_f2.append([time_, avg])
+        else:
+            vegas_f1.append([time_, avg]) if (src == 0) else vegas_f2.append([time_, avg])
+    
+    newreno_result = []
+    newreno_result.append(newreno_f1)
+    newreno_result.append(newreno_f2)
+    tahoe_result = []
+    tahoe_result.append(tahoe_f1)
+    tahoe_result.append(tahoe_f2)
+    vegas_result = []
+    vegas_result.append(vegas_f1)
+    vegas_result.append(vegas_f2)
+
+    return newreno_result, tahoe_result, vegas_result
 
 
 def calculateAvgCwnd():
     global cwnd
-    flow1_result = []
-    flow2_result = []
+    newreno_f1 = []
+    newreno_f2 = []
+    tahoe_f1 = []
+    tahoe_f2 = []
+    vegas_f1 = []
+    vegas_f2 = []
     curr_vals = [0] * n_run
     cwnd = sorted(cwnd) # sort by time
-    # for each in cwnd:
+    # for each in rtt:
     #     print(each)
     #     print("\n")
     for each in cwnd:
         time_ = float(each[0])
         cwnd_ = float(each[6])
         src = int(each[1])
+        type_ = each[7]
         run_id = int(each[len(each) - 1])
         curr_vals[run_id - 1] = cwnd_
         avg = sum(curr_vals) / len(curr_vals)
-        flow1_result.append([time_, avg]) if (src == 0) else flow2_result.append([time_, avg])
-    return flow1_result, flow2_result
+        if type_ == "Newreno":
+            newreno_f1.append([time_, avg]) if (src == 0) else newreno_f2.append([time_, avg])
+        elif type_ == "Tahoe":
+            tahoe_f1.append([time_, avg]) if (src == 0) else tahoe_f2.append([time_, avg])
+        else:
+            vegas_f1.append([time_, avg]) if (src == 0) else vegas_f2.append([time_, avg])
+    
+    newreno_result = []
+    newreno_result.append(newreno_f1)
+    newreno_result.append(newreno_f2)
+    tahoe_result = []
+    tahoe_result.append(tahoe_f1)
+    tahoe_result.append(tahoe_f2)
+    vegas_result = []
+    vegas_result.append(vegas_f1)
+    vegas_result.append(vegas_f2)
+
+    return newreno_result, tahoe_result, vegas_result
 
 
 execAllRuns()
 readAndParseAllData()
-f1_rtt, f2_rtt = calculateAvgRtt()
-f1_cwnd, f2_cwnd = calculateAvgCwnd()
+
+# each list is 3d : [flows][records][fields]
+rtt_newreno_result, rtt_tahoe_result, rtt_vegas_result = calculateAvgRtt()
+cwnd_newreno_result, cwnd_tahoe_result, cwnd_vegas_result = calculateAvgCwnd()

aa92dc29da9f3eab73c69bb394175e737d89a980
plutocholia <kiarash.nrz77@gmail.com>
2020-06-27 01:34:16 +0430

    old goodput tracker removed

--- network.tcl
+++ network.tcl
@@ -2,20 +2,6 @@ proc randGen {min max} {
     return [expr int(rand()*($max - $min + 1)) + $min] 
 }
 
-
-Class TraceApp -superclass Application     
-
-TraceApp instproc init {args} {
-    $self set bytes_ 0
-    eval $self next $args
-}
-
-TraceApp instproc recv {byte} {
-    $self instvar bytes_
-    set bytes_ [expr $bytes_ + $byte]
-    return $bytes_
-}
-
 proc doSimulation {type number verbose max_time} {
 
     global ns trace_file nam_file
@@ -75,10 +61,6 @@ proc doSimulation {type number verbose max_time} {
     set tcp5 [new Agent/TCPSink]
     set tcp6 [new Agent/TCPSink]
 
-    set traceapp5 [new TraceApp]
-    $traceapp5 attach-agent $tcp5
-    set traceapp6 [new TraceApp]
-    $traceapp6 attach-agent $tcp6
 
     $tcp1 set class_ 1
     $tcp1 set fid_ 1
@@ -99,7 +81,6 @@ proc doSimulation {type number verbose max_time} {
     $ns attach-agent $n5 $tcp5
     $ns attach-agent $n2 $tcp2
     $ns attach-agent $n6 $tcp6
-
 
     $ns connect $tcp1  $tcp5
     $ns connect $tcp2  $tcp6
@@ -132,23 +113,9 @@ proc doSimulation {type number verbose max_time} {
         }
     }
 
-    proc addGoodPutToTrace {tcpSink outfile id} {
-        global ns
-        set now [$ns now];
-        set nbytes [$tcpSink set bytes_];
-        $tcpSink set bytes_ 0;
-        set time_incr 1.0
-        set throughput [expr ($nbytes * 8.0 / 1000000) / $time_incr]
-        puts  $outfile  "#### $now $throughput $id"
-        $ns at [expr $now+$time_incr] "addGoodPutToTrace $tcpSink  $outfile $id"
-    }
 
     $ns at 0.0 "$ftp1 start"
     $ns at 0.0 "$ftp2 start"
-    $ns at 0.0 "$traceapp5 start"
-    $ns at 0.0 "$traceapp6 start"
-    $ns at 0.0 "addGoodPutToTrace $traceapp5 $trace_file 5"
-    $ns at 0.0 "addGoodPutToTrace $traceapp6 $trace_file 6"
     $ns at $max_time "$ftp1 stop"
     $ns at $max_time "$ftp2 stop"
     $ns at [expr $max_time + 0.1] "finish $nam_dir $verbose"
@@ -156,8 +123,6 @@ proc doSimulation {type number verbose max_time} {
     $ns run
 }
 
-# set types [list "Newreno" "Tahoe" "Vegas"]
-# doSimulation [lindex $types 1] 2 0
 set type [lindex $argv 0]
 set num [lindex $argv 1]
 set verbose [lindex $argv 2]
